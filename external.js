50a264a3c02973ee4ebf0d9a3ea2f47de6eae9a4
Louisaflor <louisayonzon@Louisas-MacBook-Air.local>
2021-10-27 05:36:30 -0700

    cleaned up my code
    made a new file for both JS and css and transferred the code there so my html looks cleaner

--- external.js
+++ external.js
@@ -1,3 +1,28 @@
+function sniffer(message) {
+
+    console.log("Event: "  + message);
+    alert("This is an inside alert")
+
+}
+
+alert("This is an outside alert");
+
+
 function outsideFunc() {
     alert("This is an external function!")
+}
+
+document.getElementById("textTop").onchange=function(){
+    alert("value in text box changed.");
+}
+
+document.getElementById("butt").onmouseover=function(){
+    sniffer("Moused over butt");
+}
+
+
+
+document.getElementById("secondP").onclick=function(){
+    sniffer("Clicked on secondP");
+    outsideFunc()
 }
