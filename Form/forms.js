1e96413aaccc06444bcf5b4aa65d318b559221a9
Louisaflor <louisayonzon@Louisas-MacBook-Air.local>
2021-11-28 14:29:04 -0800

    omg i thought I messed up
    NOTE TO SELF: SAVE EVERYTHING, ESPECIALLY IF I MAKE A MISTAKE!
    
    so i created a function called validateForm(), in this function we created two var, first one is the ID firstName and the sec one is the firstName value
    
    then we created an if statement stating that if the value does NOT equal to "Louisa" meaning that if the user types in the textbox and they did NOT put Louisa, the we are going to run a couple of things,...
    
    we are going to console.log saying it is not working, then we are going to grab the target ID (in this case is the <p> tag) and we are going to input a new text (innterHTML)  saying to input Louisa...
    
    then we  are going to change the text color to blue
    
    then we are going to use the select() method on firstName so when the user inputs the wrong name, it will highlight the text so it makes it easier on the user the type in the textbox
    
    so i made the mistake of doing target.select() and what was crazy was that it showed no errors yet it broke my function. so what had happened was that when I input the wrong name it still took me to the website! so yea i just have to remember those little errors..

---
+++ Form/forms.js
@@ -0,0 +1,34 @@
+document.getElementById("submit").onclick = function() {
+    var doSubmit = validateForm();
+
+    if (doSubmit == false) {
+
+        return false;
+
+    }
+
+
+}
+
+function validateForm() {
+    var firstName = document.getElementById("firstName");
+    var theValue = firstName.value;
+
+    if (theValue != "Louisa") {
+
+        console.log("This isn't working");
+
+        var target = document.getElementById("target");
+        
+        target.innerHTML = "<h2> Please Input 'Louisa' if you want this to work! </h2>"
+        target.style.color = "blue"
+
+        //before this is did target.select() and my function was not working but it did NOT show any errors
+        
+        firstName.select(); //this highlights the whole text when you put in the wrong name 
+        //you have the use firstName b/c its the one you want to select to highlight it 
+
+        return false;
+        console.log("This will only appear if you did not 'Louisa")
+    }
+}
