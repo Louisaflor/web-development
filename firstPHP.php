2d04fa932c9411b11bd3a82c616567cd15d26470
Louisaflor <louisayonzon@Louisas-MacBook-Air.local>
2021-12-10 04:50:55 -0800

    first php
    had to download mamps so this could work on the webpage
    
    for all php files it has to be stored in the the htdocs in the mamp folder

---
+++ firstPHP.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>First PHP</title>
+</head>
+
+<body>
+
+    <h3>Your first PHP code!</h3>
+
+    <?php
+
+    print("This is some PHP code!")
+
+    ?>
+
+</body>
+
+</html>
