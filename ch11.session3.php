fa03c6fde5f72565d28e44cc83395990e2abe46c
Louisaflor <louisayonzon@MacBook-Pro-2.local>
2022-01-29 11:13:42 -0800

    kinda forgot what this is
    lol uhh i kinda forgot where i left off but I just want to save the data

--- ch11.session3.php
+++ ch11.session3.php
@@ -17,32 +17,3 @@
 
 
 </html>
-
-<?php
-
-$_SESSION = ["favColor"] = "purple";
-$_SESSION = ["favAnimal"] = "seal lion";
-
-
-?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Chapter 11 Session Part 3 </title>
-
-</head>
-
-<body>
-    <h1>Session Variables created , now let's look at them</h1>
-    <?php
-
-    echo "My favorite color is: " . $_SESSION = ["favColor"] = "purple" . "<br>";
-
-    ?>
-
-
-</body>
-
-</html>
